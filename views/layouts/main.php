759963fce81a764a06b06320abb66a4aa13fc496
BaianoDev <guilhermelchaves@gmail.com>
2016-09-15 00:12:02 -0400

    Applying theme

---
+++ views/layouts/main.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title> - ReptileCode</title>
+		<!-- For responsive site
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		-->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<link rel="author" href="/humans.txt">
+		<meta name="description" content="Description Goes Here">
+		<link rel="stylesheet" href="/css/style.css">
+		<!--[if IE 7]>
+			<html class="ie7">
+			<link rel="stylesheet" type="text/css" href="/css/font-awesome-ie7.min.css">
+		<![endif]-->
+		<!--[if IE 8]><html class="ie8"> <![endif]-->
+	    <!--[if lt IE 9]>
+	      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	    <![endif]-->
+
+		<!-- <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png"> -->
+		<link rel="shortcut icon" href="ico/favicon.png">
+	</head>
+	<body>
+	<!-- Header
+	    ================================================== -->
+	<header>
+
+	</header>
+
+	<!-- Content
+    ================================================== -->
+  <div class="top-strip"></div>
+  <div class="content">
+    <main class="container">
+    	<div class="row-fluid">
+    		<section class="content">
+    		  <!-- <h1>Ipsum List</h1> -->
+          <img class="reptile-img" src="/img/reptile.jpg" width="140" />
+    		</section>
+    		<ul class="entries">
+    		
+    		  <li>
+    		    <a href="/2015/08/28/hierarchy.html">
+                      <span>28 August 2015</span>
+    		        <h3>Importing hierarchical structure based CSV</h3>
+                      <!-- <div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi...</div> -->
+    		    </a>
+    		  </li>
+    		
+    		</ul>
+    	</div>
+      <hr>
+      <div id="post-pagination">
+        
+<!--         <p class="prev disabled">
+          <span>Previous</span>
+        </p> -->
+        
+
+        <ul class="pages">
+          <li class="page">
+            
+<!--             <span class="current-page">1</span> -->
+            
+          </li>
+
+          
+        </ul>
+
+        
+<!--         <p class="next disabled">
+          <span>Next</span>
+        </p> -->
+        
+      </div>
+
+    </main>
+  </div>
+
+
+	<footer>
+		<div class="container">
+			<a href="https://github.com/vtorres/">Vitor Torres</a> | <a href="http://vtorres.github.io/">ReptileCode</a>
+		</div>
+	</footer>
+
+	<!-- Footer
+	    ================================================== -->
+
+	<!-- Javascripts
+	    ================================================= -->
+	<script src="/js/jquery.min.js"></script>
+	<script src="/js/custom.js"></script>
+
+    <!-- Analytics
+    ================================================== -->
+    <script>
+		// analytics code
+    </script>
+	</body>
+</html>
